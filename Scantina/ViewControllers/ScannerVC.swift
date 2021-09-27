efcdc321eb933c58759f27212c973f91659d0238
Danny Copeland <danny@codecantina.com>
2021-09-26 20:30:21 -0700

    removed storyboard

--- Scantina/ViewControllers/ScannerVC.swift
+++ Scantina/ViewControllers/ScannerVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ScannerVC.swift
 //  Scantina
 //
 //  Created by Danny Copeland on 9/26/21.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ScannerVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
     }
 
 
 }
-
